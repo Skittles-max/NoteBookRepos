9c30ac257c425fa3161e7c3030a1fbc47546d5b3
Skittles-max <lunatik-12345@yandex.ru>
2025-02-18 16:32:13 +0300

    add

--- DZ_Addition_list.py
+++ DZ_Addition_list.py
@@ -1,10 +1,14 @@
-
 def addition_list(list_1, list_2):
-    new_list = []
-    for i in list_1:
-        for j in list_2:
-            new_list.append(i + j)
-    print(new_list)
+    if len(list_1) > len(list_2):
+        sum_lists = list_1
+        for i in range(len(list_2)):
+            sum_lists[i] += list_2[i]
+
+    else:
+        sum_lists = list_2
+        for i in range(len(list_1)):
+            sum_lists[i] += list_1[i]
+    print(sum_lists)
 
 
-addition_list([1, 2, 3, 4, 5, 6, 7], [7, 6, 5, 4, 3, 2, 1])
+addition_list([1, 2, 3], [1, 4, 5, 6])
