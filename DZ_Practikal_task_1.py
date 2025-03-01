49cf2270705564a5c371bf9c2fb9c11f05a24933
Skittles-max <lunatik-12345@yandex.ru>
2025-03-01 16:52:22 +0300

    remove list def even_numbers

--- DZ_Practikal_task_1.py
+++ DZ_Practikal_task_1.py
@@ -3,16 +3,15 @@ def calculate_average(grades):
     return sum(grades) / len(grades)
 
 
-def overall_gpa():
+def overall_gpa(list_balls):
     list_average_balls = []
-    for grades in list_students:
+    for grades in list_balls:
         list_average_balls.append(calculate_average(grades["grades"]))
-
     print(f"Общий средний балл всех студентов: {sum(list_average_balls) / len(list_students)}")
 
 
-def average_ball():
-    for student in list_students:
+def average_ball(students):
+    for student in students:
         ball = calculate_average(student["grades"])
         name = student["name"]
         list_average_ball.append({"name": name, "average ball": ball})
@@ -23,12 +22,15 @@ def average_ball():
     return list_average_ball
 
 
-def add_delete_student():
+def add_student():
     name = input("Введите имя студента: ")
     grades_input = input("Введите список баллов через запятую: ")
     grades = [int(grade.strip()) for grade in grades_input.split(",")]
     new_student = {"name": name, "grades": grades}
     list_students.append(new_student)
+
+
+def delete_students():
     delete_name = input("Введите имя студента которого нужно удалить "
                         "из списка, если таких нет то оставьте поле пустым: ")
     if delete_name:
@@ -37,7 +39,6 @@ def add_delete_student():
                 list_students.remove(student)
     else:
         pass
-
     print(list_students)
 
 
@@ -49,6 +50,7 @@ list_students = [
     {"name": "Mahmud", "grades": [65, 85, 35, 44]}]
 
 list_average_ball = []
-average_ball()
-add_delete_student()
-overall_gpa()
+average_ball(list_students)
+add_student()
+delete_students()
+overall_gpa(list_students)
